59c7686802d4d20931885e5b7e0ee72263c194aa
syed abdul hakim <s.abdulhakim10@gmail.com>
2022-10-16 17:38:31 +0600

    created google auth provider and enable google sign in method

--- src/_step.js
+++ src/_step.js
@@ -6,4 +6,9 @@
 5. Install firebase for the project: npm install firebase
 6. DANGEROUS: get firebase config and put it in firebase.init.js
 7. export app from firebase.init.js
+8. create auth using getAuth from firebase by using app from firebase.init.js
+9. crate a google auth provider. do not forgot to use new GoogleAuthProvider();
+10. go to firebase > Build > Authentication > Sign In method
+11. Enable google sign in method
+12. create a button for google sign in method with a click handler
 */
